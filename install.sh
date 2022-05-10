a8af15dfd456183e4df48d6245e84841118767e4
DwarfMaster <luc@dwarfmaster.net>
2022-05-10 16:46:32 +0200

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+zlib_path=$(pkg-config zlib --cflags)
+zlib_path=${zlib_path#"-I"}
+zlib_include=$(pkg-config zlib --libs | cut -d ' ' -f 1)
+zlib_include=${zlib_include#"-L"}
+
+gmp_path=$(pkg-config gmp --cflags)
+gmp_path=${gmp_path#"-I"}
+gmp_include=$(pkg-config gmp --libs | cut -d ' ' -f 1)
+gmp_include=${gmp_include#"-L"}
+
+LIBRARY_PATH="$zlib_include:$gmp_include" CPATH="$zlib_path:$gmp_path" opam install coq
