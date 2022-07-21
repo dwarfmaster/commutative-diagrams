f60b27d63c0ccb30fb4fff45d8df50ef13d8e4c6
DwarfMaster <luc@dwarfmaster.net>
2022-07-21 15:53:47 +0200

    Add equality predicates for data

--- src/data.ml
+++ src/data.ml
@@ -25,17 +25,22 @@ type morphismData =
   { obj : EConstr.t
   ; tp  : morphismT
   }
+type morphismBase =
+  { data : morphismData
+  ; id   : mph_id
+  }
 type isoData =
   { obj : EConstr.t
   ; mph : morphismBase
   ; inv : morphismBase
   }
-and morphismBase =
-  { data : morphismData
-  ; id   : mph_id
-  ; mutable mono : EConstr.t option
-  ; mutable epi  : EConstr.t option
-  ; mutable iso  : isoData option
+type epiData =
+  { obj : EConstr.t
+  ; mph : morphismBase
+  }
+type monoData =
+  { obj : EConstr.t 
+  ; mph : morphismBase 
   }
 type morphismShape =
   | Base of morphismBase
@@ -82,3 +87,61 @@ type face =
   ; id    : face_id
   }
 
+let comp_category (cmp : EConstr.t -> EConstr.t -> bool) (x1 : category) (x2 : category) : bool =
+  x1.id == x2.id
+
+let comp_elem (cmp : EConstr.t -> EConstr.t -> bool) (x1 : elem) (x2 : elem) : bool =
+  x1.id == x2.id
+
+let comp_morphismT (cmp : EConstr.t -> EConstr.t -> bool) (x1 : morphismT) (x2 : morphismT) : bool =
+  comp_category cmp x1.category x2.category
+  && comp_elem cmp x1.src x2.src
+  && comp_elem cmp x1.dst x2.dst
+  && cmp x1.obj x2.obj
+
+let comp_morphismData (cmp : EConstr.t -> EConstr.t -> bool) (x1 : morphismData) (x2 : morphismData) : bool =
+  cmp x1.obj x2.obj
+
+let comp_morphismBase (cmp : EConstr.t -> EConstr.t -> bool) (x1 : morphismBase) (x2 : morphismBase) : bool =
+  x1.id == x2.id
+
+let comp_isoData (cmp : EConstr.t -> EConstr.t -> bool) (x1 : isoData) (x2 : isoData) : bool =
+  cmp x1.obj x2.obj
+let comp_epiData (cmp : EConstr.t -> EConstr.t -> bool) (x1 : epiData) (x2 : epiData) : bool =
+  cmp x1.obj x2.obj
+let comp_monoData (cmp : EConstr.t -> EConstr.t -> bool) (x1 : monoData) (x2 : monoData) : bool =
+  cmp x1.obj x2.obj
+
+let comp_morphismShape (cmp : EConstr.t -> EConstr.t -> bool) (x1 : morphismShape) (x2 : morphismShape) : bool =
+  match x1, x2 with
+  | Base x1, Base x2 -> comp_morphismBase cmp x1 x2
+
+let comp_morphism (cmp : EConstr.t -> EConstr.t -> bool) (x1 : morphism) (x2 : morphism) : bool =
+  comp_morphismData cmp x1.data x2.data
+
+let rec comp_eqT (cmp : EConstr.t -> EConstr.t -> bool) (x1 : eqT) (x2 : eqT) : bool =
+  match x1, x2 with
+  | Refl m1, Refl m2 -> comp_morphismData cmp m1 m2 
+  | Concat (eq1,eq1'), Concat (eq2,eq2') -> comp_eq cmp eq1 eq2 && comp_eq cmp eq1' eq2'
+  | Inv eq1, Inv eq2 -> comp_eq cmp eq1 eq2
+  | Compose (eq1,eq1'), Compose (eq2,eq2') -> comp_eq cmp eq1 eq2 && comp_eq cmp eq1' eq2'
+  | Assoc (m1,m1',m1''), Assoc (m2,m2',m2'') -> comp_morphismData cmp m1 m2 
+                                             && comp_morphismData cmp m1' m2' 
+                                             && comp_morphismData cmp m1'' m2''
+  | LeftId m1, LeftId m2 -> comp_morphismData cmp m1 m2
+  | RightId m1, RightId m2 -> comp_morphismData cmp m1 m2
+  | RAp (eq1,m1), RAp (eq2,m2) -> comp_morphismData cmp m1 m2 && comp_eq cmp eq1 eq2
+  | LAp (m1,eq1), LAp (m2,eq2) -> comp_morphismData cmp m1 m2 && comp_eq cmp eq1 eq2
+  | RInv iso1, RInv iso2 -> comp_isoData cmp iso1 iso2
+  | LInv iso1, LInv iso2 -> comp_isoData cmp iso1 iso2
+  | Mono (mono1,_,_,eq1), Mono (mono2,_,_,eq2) -> cmp mono1 mono2 && comp_eq cmp eq1 eq2
+  | Epi (epi1,_,_,eq1), Mono (epi2,_,_,eq2) -> cmp epi1 epi2 && comp_eq cmp eq1 eq2
+  | Atom eq1, Atom eq2 -> cmp eq1 eq2
+  | _, _ -> false
+and comp_eq (cmp : EConstr.t -> EConstr.t -> bool) (x1 : eq) (x2 : eq) : bool =
+  comp_eqT cmp x1.eq x2.eq
+
+let comp_path (cmp : EConstr.t -> EConstr.t -> bool) (x1 : path) (x2 : path) : bool =
+  comp_morphismData cmp x1.mph x2.mph
+let comp_face (cmp : EConstr.t -> EConstr.t -> bool) (x1 : face) (x2 : face) : bool =
+  comp_eq cmp x1.obj x2.obj
--- src/data.mli
+++ src/data.mli
@@ -10,44 +10,54 @@ type category =
   { obj : EConstr.t
   ; id  : cat_id
   }
+val comp_category : (EConstr.t -> EConstr.t -> bool) -> category -> category -> bool
 type elem =
   { obj      : EConstr.t
   ; category : category
   ; id       : elem_id
   }
+val comp_elem : (EConstr.t -> EConstr.t -> bool) -> elem -> elem -> bool
 type morphismT =
   { category : category
   ; src      : elem
   ; dst      : elem
   ; obj      : EConstr.t
   }
+val comp_morphismT : (EConstr.t -> EConstr.t -> bool) -> morphismT -> morphismT -> bool
 type morphismData =
   { obj : EConstr.t
   ; tp  : morphismT
   }
+val comp_morphismData : (EConstr.t -> EConstr.t -> bool) -> morphismData -> morphismData -> bool
 type morphismBase =
   { data : morphismData
   ; id   : mph_id
   }
+val comp_morphismBase : (EConstr.t -> EConstr.t -> bool) -> morphismBase -> morphismBase -> bool
 type isoData =
   { obj : EConstr.t
   ; mph : morphismBase
   ; inv : morphismBase
   }
+val comp_isoData : (EConstr.t -> EConstr.t -> bool) -> isoData -> isoData -> bool
 type epiData =
   { obj : EConstr.t
   ; mph : morphismBase
   }
+val comp_epiData : (EConstr.t -> EConstr.t -> bool) -> epiData -> epiData -> bool
 type monoData =
   { obj : EConstr.t 
   ; mph : morphismBase 
   }
+val comp_monoData : (EConstr.t -> EConstr.t -> bool) -> monoData -> monoData -> bool
 type morphismShape =
   | Base of morphismBase
+val comp_morphismShape : (EConstr.t -> EConstr.t -> bool) -> morphismShape -> morphismShape -> bool
 type morphism =
   { data  : morphismData
   ; shape : morphismShape
   }
+val comp_morphism : (EConstr.t -> EConstr.t -> bool) -> morphism -> morphism -> bool
 
 val fromBase : morphismBase -> morphism 
 
@@ -73,6 +83,8 @@ and eq =
   ; tp  : morphismT
   ; eq  : eqT
   }
+val comp_eqT : (EConstr.t -> EConstr.t -> bool) -> eqT -> eqT -> bool
+val comp_eq : (EConstr.t -> EConstr.t -> bool) -> eq -> eq -> bool
 
 (* The composed morphism of the path may not be in the context since we only keep the base *)
 type path =
@@ -80,6 +92,7 @@ type path =
   ; eq   : eq (* Equality from `mph` to `realize path` *)
   ; path : morphism list
   }
+val comp_path : (EConstr.t -> EConstr.t -> bool) -> path -> path -> bool
 type face =
   { tp    : morphismT
   ; side1 : path
@@ -87,4 +100,5 @@ type face =
   ; obj   : eq (* Equality between side1.mph and side2.mph *)
   ; id    : face_id
   }
+val comp_face : (EConstr.t -> EConstr.t -> bool) -> face -> face -> bool
 
