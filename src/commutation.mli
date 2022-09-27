91370d1e692abaecbe57e389b6891d92e7e2a09c
DwarfMaster <luc@dwarfmaster.net>
2022-09-27 21:29:57 +0200

    Comment code still to be refactored

--- src/commutation.mli
+++ src/commutation.mli
@@ -1,8 +1,8 @@
 
-type t
-
-(* Enumerate all paths and initialize the solver with those paths *)
-(* Then add all the equalities *)
-val build : Hyps.t -> int -> t Proofview.tactic
-val query : Data.path -> Data.path -> t -> Data.eq option Proofview.tactic
-val normalize_iso_in_path : Data.path -> Data.path Proofview.tactic
+(* type t *)
+(**)
+(* (* Enumerate all paths and initialize the solver with those paths *) *)
+(* (* Then add all the equalities *) *)
+(* val build : Hyps.t -> int -> t Proofview.tactic *)
+(* val query : Data.path -> Data.path -> t -> Data.eq option Proofview.tactic *)
+(* val normalize_iso_in_path : Data.path -> Data.path Proofview.tactic *)
--- src/unionFind.mli
+++ src/unionFind.mli
@@ -1,11 +1,11 @@
 
 (* t is mutable *)
-type t
-type query = Data.elem * (Data.morphism,query) Data.pathComponent list
-
-val mkQuery : Data.path -> query
-
-val init       : query list -> t Proofview.tactic
-(* Returns false if nothing was done *)
-val connect    : query -> query -> Data.eq -> t -> bool Proofview.tactic
-val query_conn : query -> query -> t -> Data.eq option Proofview.tactic
+(* type t *)
+(* type query = Data.elem * (Data.morphism,query) Data.pathComponent list *)
+(**)
+(* val mkQuery : Data.path -> query *)
+(**)
+(* val init       : query list -> t Proofview.tactic *)
+(* (* Returns false if nothing was done *) *)
+(* val connect    : query -> query -> Data.eq -> t -> bool Proofview.tactic *)
+(* val query_conn : query -> query -> t -> Data.eq option Proofview.tactic *)
