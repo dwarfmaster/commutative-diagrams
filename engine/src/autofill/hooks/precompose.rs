719dca24eaa84403d37ce9c53c8a00e6865647ad
DwarfMaster <luc@dwarfmaster.net>
2023-02-06 16:19:06 +0100

    [engine] Fix precomposition with identity

--- engine/src/autofill/hooks/precompose.rs
+++ engine/src/autofill/hooks/precompose.rs
@@ -1,8 +1,14 @@
-use crate::data::{ActualEquality, Context, Equality, Morphism};
+use crate::data::{ActualEquality, Context, Equality, Morphism, ActualMorphism};
+use crate::normalize;
+use crate::dsl::{eq,mph};
 
 pub fn hook(mph: Morphism, ctx: &mut Context, eq: Equality, opts: &mut Vec<Equality>) {
     if mph.dst(ctx) == eq.src(ctx) {
+        let (_, eql) = normalize::morphism(ctx, mph!(ctx, mph >> (eq.left(ctx))));
+        let (_, eqr) = normalize::morphism(ctx, mph!(ctx, mph >> (eq.right(ctx))));
         let eq = ctx.mk(ActualEquality::LAp(mph.clone(), eq));
+        let eq = eq!(ctx, (~eql) . (eq . eqr));
+        assert!(eq.check(ctx), "Invalid equality produced");
         opts.push(eq)
     }
 }
