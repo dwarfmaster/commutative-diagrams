e20d54de81de1a43953393b05765386db399e6fb
DwarfMaster <luc@dwarfmaster.net>
2023-02-22 13:18:19 +0100

    [engine] When splitting an edge, hide it

--- engine/src/vm/actions/split.rs
+++ engine/src/vm/actions/split.rs
@@ -1,10 +1,16 @@
 use crate::tactics;
+use crate::vm::graph::GraphId;
 use crate::vm::VM;
 
 impl VM {
     pub fn split(&mut self, src: usize, mph: usize) {
-        // TODO hide previous morphism
-        tactics::split_norm(&mut self.ctx, &mut self.graph, src, mph);
-        self.layout()
+        // TODO create a display system for equality sides
+        if let Some(fce) = tactics::split_norm(&mut self.ctx, &mut self.graph, src, mph) {
+            if self.graph.faces[fce].label.label.is_empty() {
+                self.graph.faces[fce].label.label =
+                    self.graph.faces[fce].eq.render(&mut self.ctx, 100);
+            }
+            self.hide(GraphId::Morphism(src, mph))
+        }
     }
 }
