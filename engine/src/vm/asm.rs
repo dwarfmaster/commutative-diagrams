647cf4400e7d310ba8da5f168ee77391ab59e3e1
DwarfMaster <luc@dwarfmaster.net>
2023-03-01 11:19:13 +0100

    [engine] Use lens to provide easier asm::Updater creation

--- engine/src/vm/asm.rs
+++ engine/src/vm/asm.rs
@@ -1,6 +1,7 @@
 use crate::data::{Equality, Morphism, Object};
 use crate::graph::Face;
 use crate::vm::{EdgeLabel, FaceLabel, NodeLabel};
+use lens_rs::LensMut;
 use std::fmt::{Debug, Error, Formatter};
 
 pub struct Updater<T> {
@@ -24,6 +25,25 @@ impl<T> Updater<T> {
 
     pub fn inverse(&mut self) {
         std::mem::swap(&mut self.direct, &mut self.reverse);
+    }
+
+    pub fn from_lens<L, O>(val: &T, lens: L, new: O) -> Self
+    where
+        T: LensMut<L, O>,
+        O: Clone + Send + Sync + 'static,
+        L: Copy + Send + Sync + 'static,
+    {
+        let old = val.view_ref(lens).clone();
+        let direct = move |v: &mut T| {
+            *v.view_mut(lens) = new.clone();
+        };
+        let reverse = move |v: &mut T| {
+            *v.view_mut(lens) = old.clone();
+        };
+        Self {
+            direct: Box::new(direct),
+            reverse: Box::new(reverse),
+        }
     }
 }
 
--- engine/src/vm/graph.rs
+++ engine/src/vm/graph.rs
@@ -1,4 +1,5 @@
 use crate::graph;
+use lens_rs::Lens;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum GraphId {
@@ -15,13 +16,18 @@ pub enum LabelSource {
     None,
 }
 
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, Lens)]
 pub struct NodeLabel {
+    #[optic]
     pub pos: egui::Pos2,
+    #[optic]
     pub name: Option<String>,
+    #[optic]
     pub label: String,
+    #[optic]
     pub label_source: LabelSource,
     // Invariant: when a node is hidden, all in/out-going edges must be hidden too
+    #[optic]
     pub hidden: bool,
 }
 
@@ -38,22 +44,33 @@ impl NodeLabel {
 }
 
 // Default is all false
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Lens)]
 pub struct EdgeStyle {
+    #[optic]
     pub left: bool,
+    #[optic]
     pub right: bool,
+    #[optic]
     pub highlight: bool,
 }
 
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, Lens)]
 pub struct EdgeLabel {
+    #[optic]
     pub shape: Vec<[egui::Pos2; 4]>,
+    #[optic]
     pub name: Option<String>,
+    #[optic]
     pub id: usize,
+    #[optic]
     pub label: String,
+    #[optic]
     pub label_pos: egui::Pos2,
+    #[optic]
     pub label_source: LabelSource,
+    #[optic]
     pub style: EdgeStyle,
+    #[optic]
     pub hidden: bool,
 }
 
@@ -72,14 +89,20 @@ impl EdgeLabel {
     }
 }
 
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, Lens)]
 pub struct FaceLabel {
+    #[optic]
     pub label: String,
+    #[optic]
     pub label_source: LabelSource,
+    #[optic]
     pub name: Option<String>,
+    #[optic]
     pub hidden: bool,
     // The paths to show for this equality
+    #[optic]
     pub left: Option<Vec<usize>>,
+    #[optic]
     pub right: Option<Vec<usize>>,
 }
 
