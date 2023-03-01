49247a76ee8ad0f2c547fab2d69f1bfe4d917738
DwarfMaster <luc@dwarfmaster.net>
2023-03-01 11:43:13 +0100

    [engine] Rewrite labeling as a compilable action

--- engine/src/vm/actions/label.rs
+++ engine/src/vm/actions/label.rs
@@ -1,6 +1,10 @@
-use crate::vm::graph::LabelSource;
+use crate::vm::asm;
+use crate::vm::graph::{GraphId, LabelSource};
 use crate::vm::VM;
 use hashconsing::HConsed;
+use lens_rs::{optics, LensMut, Optics};
+
+type Ins = asm::Instruction;
 
 fn should_render<T>(src: &LabelSource, obj: &HConsed<T>) -> bool {
     use LabelSource::*;
@@ -11,35 +15,57 @@ fn should_render<T>(src: &LabelSource, obj: &HConsed<T>) -> bool {
     }
 }
 
+fn label_upd<Label>(uid: u64, label: &Label, text: String) -> asm::Updater<Label>
+where
+    Label: LensMut<Optics![label], String> + LensMut<Optics![label_source], LabelSource>,
+{
+    let direct = move |lbl: &mut Label| {
+        *lbl.view_mut(optics!(label)) = text.clone();
+        *lbl.view_mut(optics!(label_source)) = LabelSource::Render(uid);
+    };
+    let prev_label = label.view_ref(optics!(label)).clone();
+    let prev_label_source = label.view_ref(optics!(label_source)).clone();
+    let reverse = move |lbl: &mut Label| {
+        *lbl.view_mut(optics!(label)) = prev_label.clone();
+        *lbl.view_mut(optics!(label_source)) = prev_label_source.clone();
+    };
+    asm::Updater {
+        direct: Box::new(direct),
+        reverse: Box::new(reverse),
+    }
+}
+
 impl VM {
     pub fn relabel(&mut self) {
-        use LabelSource::*;
-        let mut relayout = false;
-        for (nd, lbl) in self.graph.nodes.iter_mut() {
+        for id in 0..self.graph.nodes.len() {
+            let (nd, lbl) = &self.graph.nodes[id];
             if should_render(&lbl.label_source, &nd) {
-                lbl.label = nd.render(&mut self.ctx, 100);
-                lbl.label_source = Render(nd.uid());
-                relayout = true;
+                let nlbl = nd.render(&mut self.ctx, 100);
+                self.register_instruction(Ins::UpdateNodeLabel(id, label_upd(nd.uid(), lbl, nlbl)));
             }
         }
         for src in 0..self.graph.nodes.len() {
-            for (_, lbl, mph) in self.graph.edges[src].iter_mut() {
+            for mph_id in 0..self.graph.edges[src].len() {
+                let (_, lbl, mph) = &self.graph.edges[src][mph_id];
                 if should_render(&lbl.label_source, &mph) {
-                    lbl.label = mph.render(&mut self.ctx, 100);
-                    lbl.label_source = Render(mph.uid());
-                    relayout = true;
+                    let nlbl = mph.render(&mut self.ctx, 100);
+                    self.register_instruction(Ins::UpdateMorphismLabel(
+                        src,
+                        mph_id,
+                        label_upd(mph.uid(), lbl, nlbl),
+                    ));
                 }
             }
         }
-        for fce in self.graph.faces.iter_mut() {
+        for id in 0..self.graph.faces.len() {
+            let fce = &self.graph.faces[id];
             if should_render(&fce.label.label_source, &fce.eq) {
-                fce.label.label = fce.eq.render(&mut self.ctx, 100);
-                fce.label.label_source = Render(fce.eq.uid());
-                relayout = true;
+                let nlbl = fce.eq.render(&mut self.ctx, 100);
+                self.register_instruction(Ins::UpdateFaceLabel(
+                    id,
+                    label_upd(fce.eq.uid(), &fce.label, nlbl),
+                ));
             }
-        }
-        if relayout {
-            self.layout();
         }
     }
 }
