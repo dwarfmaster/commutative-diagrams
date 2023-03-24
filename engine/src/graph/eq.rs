774e33ed8ec67f33fca9c437cd6f60b6022fa738
DwarfMaster <luc@dwarfmaster.net>
2023-03-24 13:11:24 +0100

    [engine] Implement insertion of block into eq slice

--- engine/src/graph/eq.rs
+++ engine/src/graph/eq.rs
@@ -1,17 +1,16 @@
-
-use crate::data::{EqualityData,Functor};
+use crate::data::{EqualityData, Functor};
 use std::cmp::Ordering;
 
 type Path = (usize, Vec<(usize, usize)>);
 
-#[derive(Eq,PartialEq,Hash,Debug,Clone)]
+#[derive(Eq, PartialEq, Hash, Debug, Clone)]
 pub struct Eq {
     pub slices: Vec<Slice>,
     pub inp: Path,
     pub outp: Path,
 }
 
-#[derive(Eq,PartialEq,Hash,Debug,Clone)]
+#[derive(Eq, PartialEq, Hash, Debug, Clone)]
 pub struct Slice {
     pub inp: Path,
     pub outp: Path,
@@ -21,57 +20,59 @@ pub struct Slice {
     pub blocks: Vec<(usize, usize, Block)>,
 }
 
-#[derive(Eq,PartialEq,Hash,Debug,Clone)]
+#[derive(Eq, PartialEq, Hash, Debug, Clone)]
 pub struct Block {
     pub inp: Path,
     pub outp: Path,
     pub data: BlockData,
 }
 
-#[derive(Eq,PartialEq,Hash,Debug,Clone)]
+#[derive(Eq, PartialEq, Hash, Debug, Clone)]
 pub enum BlockData {
     Direct(EqualityData),
     Inv(EqualityData),
     Funct(Functor, Eq),
 }
 
+// TODO handle functors
+
 impl Eq {
-    fn append_block(&mut self, blk: Block) {
-    }
+    fn append_block(&mut self, blk: Block) {}
 }
 
 impl Slice {
     // Try to insert a block. Return None on success, and give back the block on failure
     fn insert_block_at(&mut self, start: usize, blk: Block) -> Option<Block> {
-        // if !self.block_compatible(start, &blk) {
-        //     return Some(blk)
-        // }
-        //
-        // let r = self.blocks.binary_search_by_key(&start, |path| path.0).unwrap_err();
-        // TODO handle functors
-        // self.blocks.insert(r, (start,blk));
+        if !self.block_compatible(start, &blk) {
+            return Some(blk);
+        }
 
-        // inp doesn't change, since we insert on a passthrough place
+        // inp doesn't change, since we insert on a passthrough place.
         // However output changes
-        todo!()
+        let start_output = self.input_target(start).unwrap();
+        let output_range = start_output..(start_output + blk.inp.1.len());
+        self.outp.1.splice(output_range, blk.outp.1.iter().copied());
+
+        let r = self
+            .blocks
+            .binary_search_by_key(&start, |path| path.0)
+            .unwrap_err();
+        self.blocks.insert(r, (start, start_output, blk));
+        None
     }
 
     // Test if the block is compatible with the slice (assumes well typedness)
     fn block_compatible(&self, start: usize, blk: &Block) -> bool {
-        // for (st,b) in &self.blocks {
-        //     if start < st + b.outp.1.len() && *st < start + blk.inp.1.len() {
-        //         return false;
-        //     }
-        // }
-        // return true;
-        todo!()
+        (0..blk.inp.1.len())
+            .map(|x| x + start)
+            .all(|input| self.input_target(input).is_ok())
     }
 
     // Given an input index, indicates which output it connects to or if it goes
     // into a block (giving the block index and the index of the input of the
     // block).
-    pub fn input_target(&self, input: usize) -> Result<usize,(usize,usize)> {
-        let r = self.blocks.binary_search_by(|(src,_,blk)| {
+    pub fn input_target(&self, input: usize) -> Result<usize, (usize, usize)> {
+        let r = self.blocks.binary_search_by(|(src, _, blk)| {
             if *src > input {
                 Ordering::Greater
             } else if src + blk.inp.1.len() <= input {
@@ -93,8 +94,8 @@ impl Slice {
 
     // Same in the other direction: given an output give the connected input or
     // block output
-    pub fn output_source(&self, output: usize) -> Result<usize,(usize,usize)> {
-        let r = self.blocks.binary_search_by(|(_,src,blk)| {
+    pub fn output_source(&self, output: usize) -> Result<usize, (usize, usize)> {
+        let r = self.blocks.binary_search_by(|(_, src, blk)| {
             if *src > output {
                 Ordering::Greater
             } else if src + blk.outp.1.len() <= output {
@@ -117,9 +118,9 @@ impl Slice {
 
 #[cfg(test)]
 mod tests {
-    use crate::graph::eq::{Slice,Block,BlockData};
-    use crate::data::{Context,EqualityData,ActualProofObject};
-    use crate::dsl::{cat,obj,mph};
+    use crate::data::{ActualProofObject, Context, EqualityData};
+    use crate::dsl::{cat, mph, obj};
+    use crate::graph::eq::{Block, BlockData, Slice};
 
     fn dummy_data() -> BlockData {
         let ctx = Context::new();
@@ -137,41 +138,68 @@ mod tests {
         BlockData::Direct(eqdata)
     }
 
-    #[test]
-    fn slice_input_output() {
+    fn test_slice() -> Slice {
         let b31 = Block {
-            inp: (0, vec![(0,0); 3]),
-            outp: (0, vec![(0,0); 1]),
-            data: dummy_data(), 
+            inp: (0, vec![(0, 0); 3]),
+            outp: (0, vec![(0, 0); 1]),
+            data: dummy_data(),
         };
         let b14 = Block {
-            inp: (0, vec![(0,0); 1]),
-            outp: (0, vec![(0,0); 4]),
-            data: dummy_data(), 
+            inp: (0, vec![(0, 0); 1]),
+            outp: (0, vec![(0, 0); 4]),
+            data: dummy_data(),
         };
         let b25 = Block {
-            inp: (0, vec![(0,0); 2]),
-            outp: (0, vec![(0,0); 5]),
-            data: dummy_data(), 
+            inp: (0, vec![(0, 0); 2]),
+            outp: (0, vec![(0, 0); 5]),
+            data: dummy_data(),
         };
-        let slice = Slice {
-            inp: (0, vec![(0,0); 10]),
-            outp: (0, vec![(0,0); 14]),
-            blocks: vec![ (1,1,b31), (5,3,b25), (9,10,b14) ],
-        };
+        Slice {
+            inp: (0, vec![(0, 0); 10]),
+            outp: (0, vec![(0, 0); 14]),
+            blocks: vec![(1, 1, b31), (5, 3, b25), (9, 10, b14)],
+        }
+    }
+
+    #[test]
+    fn slice_input_output() {
+        let slice = test_slice();
 
         assert_eq!(slice.input_target(0), Ok(0));
-        assert_eq!(slice.input_target(2), Err((0,1)));
+        assert_eq!(slice.input_target(2), Err((0, 1)));
         assert_eq!(slice.input_target(4), Ok(2));
         assert_eq!(slice.input_target(8), Ok(9));
-        assert_eq!(slice.input_target(9), Err((2,0)));
+        assert_eq!(slice.input_target(9), Err((2, 0)));
 
         assert_eq!(slice.output_source(0), Ok(0));
-        assert_eq!(slice.output_source(1), Err((0,0)));
+        assert_eq!(slice.output_source(1), Err((0, 0)));
         assert_eq!(slice.output_source(2), Ok(4));
-        assert_eq!(slice.output_source(4), Err((1,1)));
-        assert_eq!(slice.output_source(6), Err((1,3)));
+        assert_eq!(slice.output_source(4), Err((1, 1)));
+        assert_eq!(slice.output_source(6), Err((1, 3)));
         assert_eq!(slice.output_source(8), Ok(7));
-        assert_eq!(slice.output_source(12), Err((2,2)));
+        assert_eq!(slice.output_source(12), Err((2, 2)));
+    }
+
+    #[test]
+    fn slice_insert_block() {
+        let b25 = Block {
+            inp: (0, vec![(0, 0); 2]),
+            outp: (0, vec![(1, 1); 5]),
+            data: dummy_data(),
+        };
+        let mut slice = test_slice();
+
+        let ins1 = slice.insert_block_at(1, b25.clone());
+        // Check nothing's changed
+        assert!(ins1.is_some());
+        assert_eq!(slice.inp.1.len(), 10);
+        assert_eq!(slice.outp.1.len(), 14);
+
+        let ins2 = slice.insert_block_at(7, b25);
+        assert!(ins2.is_none());
+        assert_eq!(slice.inp.1.len(), 10);
+        assert_eq!(slice.outp.1.len(), 17);
+        assert_eq!(slice.outp.1[8], (1,1));
+        assert_eq!(slice.outp.1[12], (1,1));
     }
 }
