6675e1ca2001ef9ee4bb655a41b922df841d8505
DwarfMaster <luc@dwarfmaster.net>
2023-03-31 10:24:25 +0200

    [engine] Add right-click menu on faces

--- engine/src/ui/faces.rs
+++ engine/src/ui/faces.rs
@@ -5,12 +5,14 @@ pub fn faces(ui: &mut egui::Ui, vm: &mut VM) {
 
     egui::ScrollArea::vertical().show(ui, |ui| {
         ui.radio_value(&mut vm.selected_face, None, "No face");
-        for fce in vm
-            .face_goal_order
-            .iter()
-            .copied()
-            .chain(vm.face_hyps_order.iter().copied())
-        {
+        let len = vm.face_goal_order.len() + vm.face_hyps_order.len();
+        for id in 0..len {
+            let fce = if id >= vm.face_goal_order.len() {
+                vm.face_hyps_order[id - vm.face_goal_order.len()]
+            } else {
+                vm.face_goal_order[id]
+            };
+
             if vm.graph.faces[fce].label.hidden {
                 continue;
             }
@@ -27,7 +29,33 @@ pub fn faces(ui: &mut egui::Ui, vm: &mut VM) {
                 }
                 FaceStatus::Hypothesis => ui.visuals_mut().override_text_color = None,
             }
-            ui.radio_value(&mut vm.selected_face, Some(fce), label);
+
+            let resp = ui.radio_value(&mut vm.selected_face, Some(fce), label);
+            resp.context_menu(|ui| {
+                let mut empty = true;
+                if vm.graph.faces[fce].label.status == FaceStatus::Goal {
+                    empty = false;
+                    if ui.button("Solve").clicked() {
+                        vm.insert_and_run(&format!("solve {}", vm.graph.faces[fce].label.name));
+                        ui.close_menu();
+                    }
+                    if ui.button("Shrink").clicked() {
+                        vm.insert_and_run(&format!("shrink {}", vm.graph.faces[fce].label.name));
+                        ui.close_menu();
+                    }
+                    if ui.button("Pull").clicked() {
+                        vm.insert_and_run(&format!("pull {}, *", vm.graph.faces[fce].label.name));
+                        ui.close_menu();
+                    }
+                    if ui.button("Push").clicked() {
+                        vm.insert_and_run(&format!("push {}, *", vm.graph.faces[fce].label.name));
+                        ui.close_menu();
+                    }
+                }
+                if empty {
+                    ui.close_menu();
+                }
+            });
         }
     });
 
