3fa5725eab3f3c5f0fd4b283c90182c52c4b0305
DwarfMaster <luc@dwarfmaster.net>
2023-06-19 12:46:31 +0200

    [engine] Extend TermEngine trait to support querying types

--- engine/src/remote/engine.rs
+++ engine/src/remote/engine.rs
@@ -2,6 +2,13 @@ pub trait TermEngine {
     type R: super::Remote;
 
     fn remote<'a>(&'a mut self) -> &'a mut Self::R;
+
+    fn is_cat(&mut self, obj: u64) -> bool;
+    fn is_obj(&mut self, obj: u64, cat: u64) -> bool;
+    fn is_funct(&mut self, obj: u64, cat: u64) -> Option<u64>;
+    fn is_mph(&mut self, obj: u64, cat: u64) -> Option<(u64, u64)>;
+    fn is_eq(&mut self, obj: u64, cat: u64) -> Option<(u64, u64, u64, u64)>;
+
     fn is_funct_obj(&mut self, obj: u64, cat: u64) -> Option<(u64, u64, u64)>;
     fn is_identity(&mut self, obj: u64, cat: u64) -> Option<u64>;
     fn is_comp(&mut self, obj: u64, cat: u64) -> Option<(u64, u64, u64, u64, u64)>;
