31d0d67b1f9b7cb79cb0ed0c054e17bfd47ee70b
DwarfMaster <luc@dwarfmaster.net>
2023-06-23 10:01:51 +0200

    [coq] Fix creation of goal evar

--- coq/plugin/extractor.ml
+++ coq/plugin/extractor.ml
@@ -67,11 +67,12 @@ let extract_hyps (goal : Proofview.Goal.t)
   let* env = env () in
   let context = Proofview.Goal.hyps goal in
   let concl = Proofview.Goal.concl goal in
-  let goal = Proofview.Goal.goal goal |> fun ev -> EConstr.mkEvar (ev,[]) in
+  let* sigma = evars () in
+  (* TODO! remember this is the one the goal should be refined with *)
+  let* goal = Build.mk_evar env sigma concl in
   let* is_eq = Query.query global_ns env Features.Tag.Equality goal concl in
   match is_eq with
   | Some _ ->
-      let* sigma = evars () in
       let* tptp = Query.get_type global_ns env sigma concl in
       let* tpobj = Hyps.registerObj global_ns concl tptp None in
       let* obj = Hyps.registerObj global_ns goal concl (Some "Goal") in
