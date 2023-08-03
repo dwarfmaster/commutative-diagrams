9b97102bcdde8d63137f9bcb0cba46031f9f8246
DwarfMaster <luc@dwarfmaster.net>
2023-08-03 13:06:27 +0200

    [engine] Fix unification of equalities

--- engine/src/vm/actions/unif_eq.rs
+++ engine/src/vm/actions/unif_eq.rs
@@ -1,11 +1,37 @@
 use super::super::vm::{Interactive, VM};
+use crate::data::EvarStatus;
 use crate::graph::eq::Eq;
 use crate::realizer::realize_eq;
 use crate::remote::Remote;
+use crate::vm::Context;
+
+// Check if the equality is simple, and furthermore checks if the internal object
+// is an evar. Otherwise returns None. Returns Ok if the evar is directly used and
+// Err if it is inversed.
+fn eq_is_evar<Rm: Remote + Sync + Send>(
+    ctx: &mut Context<Rm>,
+    eq: &Eq,
+) -> Option<Result<u64, u64>> {
+    if let Some(e) = eq.is_simple() {
+        match e {
+            Ok(a) => {
+                if ctx.get_stored_status(a) == EvarStatus::Evar {
+                    return Some(Ok(a));
+                }
+            }
+            Err(a) => {
+                if ctx.get_stored_status(a) == EvarStatus::Evar {
+                    return Some(Err(a));
+                }
+            }
+        }
+    }
+    None
+}
 
 impl<Rm: Remote + Sync + Send, I: Interactive + Sync + Send> VM<Rm, I> {
     pub fn unify_eq(&mut self, cat: u64, eq1: &Eq, eq2: &Eq) -> bool {
-        let (atomic, eq) = if let Some(e) = eq1.is_simple() {
+        let (atomic, eq) = if let Some(e) = eq_is_evar(&mut self.ctx, eq1) {
             match e {
                 Ok(a) => (a, eq2.clone()),
                 Err(a) => {
@@ -14,7 +40,7 @@ impl<Rm: Remote + Sync + Send, I: Interactive + Sync + Send> VM<Rm, I> {
                     (a, eq)
                 }
             }
-        } else if let Some(e) = eq2.is_simple() {
+        } else if let Some(e) = eq_is_evar(&mut self.ctx, eq2) {
             match e {
                 Ok(a) => (a, eq1.clone()),
                 Err(a) => {
