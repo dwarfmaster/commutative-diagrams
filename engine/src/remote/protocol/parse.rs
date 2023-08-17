b7e0b19608dfb4e4bdb3c032e7e86c2e1421d295
DwarfMaster <luc@dwarfmaster.net>
2023-08-17 12:30:53 +0200

    [engine] Fix protocol handler for parse response

--- engine/src/remote/protocol/parse.rs
+++ engine/src/remote/protocol/parse.rs
@@ -1,4 +1,4 @@
-use serde::de::Visitor;
+use serde::de::{Error, Unexpected, Visitor};
 use serde::{Deserialize, Deserializer, Serialize};
 
 pub struct ParseResult {
@@ -10,6 +10,32 @@ impl ParseResult {
     }
 }
 
+macro_rules! make_signed_visitor {
+    ($name:ident, $t:ty) => {
+        fn $name<E: Error>(self, v: $t) -> Result<Self::Value, E> {
+            if v < 0 {
+                Err(E::invalid_value(
+                    Unexpected::Signed(v as i64),
+                    &"a positive number",
+                ))
+            } else {
+                Ok(ParseResult {
+                    content: Ok(v as u64),
+                })
+            }
+        }
+    };
+}
+macro_rules! make_unsigned_visitor {
+    ($name:ident, $t:ty) => {
+        fn $name<E>(self, v: $t) -> Result<Self::Value, E> {
+            Ok(ParseResult {
+                content: Ok(v as u64),
+            })
+        }
+    };
+}
+
 struct ParseResultVisitor {}
 impl<'de> Visitor<'de> for ParseResultVisitor {
     type Value = ParseResult;
@@ -18,9 +44,14 @@ impl<'de> Visitor<'de> for ParseResultVisitor {
         write!(formatter, "a string or a number")
     }
 
-    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E> {
-        Ok(ParseResult { content: Ok(v) })
-    }
+    make_signed_visitor!(visit_i8, i8);
+    make_signed_visitor!(visit_i16, i16);
+    make_signed_visitor!(visit_i32, i32);
+    make_signed_visitor!(visit_i64, i64);
+    make_unsigned_visitor!(visit_u8, u8);
+    make_unsigned_visitor!(visit_u16, u16);
+    make_unsigned_visitor!(visit_u32, u32);
+    make_unsigned_visitor!(visit_u64, u64);
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> {
         Ok(ParseResult {
