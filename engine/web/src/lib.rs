3b2e31775e4d9988339c276abfcf2a627b399fe3
DwarfMaster <luc@dwarfmaster.net>
2023-09-21 15:45:10 +0200

    [engine/web] Setup egui for web

--- engine/web/src/lib.rs
+++ engine/web/src/lib.rs
@@ -1,11 +1,82 @@
+use eframe::WebRunner;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
+// Handle to the web app from javascript
+#[derive(Clone)]
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello {}", name));
+pub struct WebHandle {
+    runner: WebRunner,
 }
+
+#[wasm_bindgen]
+impl WebHandle {
+    #[allow(clippy::new_without_default)]
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        // Redirect log messages to console.log
+        eframe::WebLogger::init(log::LevelFilter::Trace).ok();
+        Self {
+            runner: WebRunner::new(),
+        }
+    }
+
+    #[wasm_bindgen]
+    pub async fn start(&self, canvas_id: &str) -> Result<(), wasm_bindgen::JsValue> {
+        self.runner
+            .start(
+                canvas_id,
+                eframe::WebOptions::default(),
+                Box::new(|_cc| Box::new(EguiApp::new())),
+            )
+            .await
+    }
+
+    #[wasm_bindgen]
+    pub fn destroy(&self) {
+        self.runner.destroy()
+    }
+
+    #[wasm_bindgen]
+    pub fn has_panicked(&self) -> bool {
+        self.runner.has_panicked()
+    }
+
+    #[wasm_bindgen]
+    pub fn panic_message(&self) -> Option<String> {
+        self.runner.panic_summary().map(|s| s.message())
+    }
+
+    #[wasm_bindgen]
+    pub fn panic_callstack(&self) -> Option<String> {
+        self.runner.panic_summary().map(|s| s.callstack())
+    }
+}
+
+struct EguiApp {
+    input: String,
+}
+
+impl EguiApp {
+    pub fn new() -> Self {
+        Self { 
+            input: String::new(),
+        }
+    }
+}
+
+impl eframe::App for EguiApp {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        eframe::egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add(eframe::egui::TextEdit::singleline(&mut self.input));
+            if ui.button("Test").clicked() {
+                alert(&format!("Hello {}", self.input))
+            }
+        });
+    }
+}
+
