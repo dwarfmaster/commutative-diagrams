bddaeae77c6e1fe38d0272ea9275e3d6477f2775
DwarfMaster <luc@dwarfmaster.net>
2023-09-22 12:36:26 +0200

    [engine/web] Setup websocket communication between the server and the interface

--- engine/server/src/main.rs
+++ engine/server/src/main.rs
@@ -1,11 +1,29 @@
 use warp::http::Uri;
 use warp::Filter;
+// use futures_util::StreamExt;
+use futures_util::sink::SinkExt;
+use futures_util::FutureExt;
 
 #[tokio::main]
 async fn main() {
     let static_assets = warp::path("static").and(warp::fs::dir("../web"));
     let root = warp::path::end().map(|| warp::redirect(Uri::from_static("/static/index.html")));
     let script = warp::path("scripts").and(warp::fs::dir("../web"));
-    let routes = static_assets.or(root).or(script);
+    let websocket = warp::path("wss")
+        .and(warp::ws())
+        .and(warp::path::param())
+        .and_then(ws_handler);
+    let routes = static_assets.or(root).or(script).or(websocket);
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+}
+
+pub async fn ws_handler(ws: warp::ws::Ws, id: String) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(ws.on_upgrade(|socket| client_connection(socket, id)))
+}
+
+pub async fn client_connection(mut ws: warp::ws::WebSocket, id: String) {
+    // let (ws_snd, mut ws_rcv) = ws.split();
+    ws.send(warp::ws::Message::text(format!("Hello {}\n", id)))
+        .map(|_result| ())
+        .await
 }
--- engine/web/src/lib.rs
+++ engine/web/src/lib.rs
@@ -2,10 +2,16 @@ use commutative_diagrams_engine_lib::{data, graph, remote, ui};
 use eframe::WebRunner;
 use std::time::Duration;
 use wasm_bindgen::prelude::*;
+use web_sys::{MessageEvent, WebSocket};
 
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
 
 // Handle to the web app from javascript
@@ -33,7 +39,65 @@ impl WebHandle {
             .start(
                 canvas_id,
                 eframe::WebOptions::default(),
-                Box::new(|_cc| Box::new(EguiApp::new())),
+                Box::new(|_cc| {
+                    let ws = WebSocket::new("ws://127.0.0.1:8000/wss/test").unwrap_or_else(|err| {
+                        console_log!("Error: {:?}", err);
+                        panic!();
+                    });
+                    ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
+                    console_log!("Connected to {}", ws.url());
+                    let cloned_ws = ws.clone();
+                    let onmessage_callback =
+                        Closure::<dyn FnMut(_)>::new(move |e: MessageEvent| {
+                            // Handle difference Text/Binary,...
+                            if let Ok(abuf) = e.data().dyn_into::<js_sys::ArrayBuffer>() {
+                                console_log!("message event, received arraybuffer: {:?}", abuf);
+                                let array = js_sys::Uint8Array::new(&abuf);
+                                let len = array.byte_length() as usize;
+                                console_log!(
+                                    "Arraybuffer received {}bytes: {:?}",
+                                    len,
+                                    array.to_vec()
+                                );
+                                // here you can for example use Serde Deserialize decode the message
+                                // for demo purposes we switch back to Blob-type and send off another binary message
+                                cloned_ws.set_binary_type(web_sys::BinaryType::Blob);
+                                match cloned_ws.send_with_u8_array(&[5, 6, 7, 8]) {
+                                    Ok(_) => console_log!("binary message successfully sent"),
+                                    Err(err) => console_log!("error sending message: {:?}", err),
+                                }
+                            } else if let Ok(blob) = e.data().dyn_into::<web_sys::Blob>() {
+                                console_log!("message event, received blob: {:?}", blob);
+                                // better alternative to juggling with FileReader is to use https://crates.io/crates/gloo-file
+                                let fr = web_sys::FileReader::new().unwrap();
+                                let fr_c = fr.clone();
+                                // create onLoadEnd callback
+                                let onloadend_cb = Closure::<dyn FnMut(_)>::new(
+                                    move |_e: web_sys::ProgressEvent| {
+                                        let array =
+                                            js_sys::Uint8Array::new(&fr_c.result().unwrap());
+                                        let len = array.byte_length() as usize;
+                                        console_log!(
+                                            "Blob received {}bytes: {:?}",
+                                            len,
+                                            array.to_vec()
+                                        );
+                                        // here you can for example use the received image/png data
+                                    },
+                                );
+                                fr.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
+                                fr.read_as_array_buffer(&blob).expect("blob not readable");
+                                onloadend_cb.forget();
+                            } else if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
+                                console_log!("message event, received Text: {:?}", txt);
+                            } else {
+                                console_log!("message event, received Unknown: {:?}", e.data());
+                            }
+                        });
+                    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+                    onmessage_callback.forget();
+                    Box::new(EguiApp::new(ws))
+                }),
             )
             .await
     }
@@ -64,10 +128,11 @@ type VM = ui::VM<RPC>;
 
 struct EguiApp {
     vm: VM,
+    ws: WebSocket,
 }
 
 impl EguiApp {
-    pub fn new() -> Self {
+    pub fn new(ws: WebSocket) -> Self {
         use data::EvarStatus::Grounded;
         use data::Feature;
         use graph::{FaceParsed, GraphParsed};
@@ -143,7 +208,11 @@ impl EguiApp {
             faces: vec![face],
         };
         ctx.set_graph(gr);
-        Self { vm: VM::start(ctx) }
+
+        Self {
+            vm: VM::start(ctx),
+            ws,
+        }
     }
 }
 
