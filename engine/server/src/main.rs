839b576c4cfcf06a33df5dd4e2d1d3ae4a5809b9
DwarfMaster <luc@dwarfmaster.net>
2023-09-28 13:42:36 +0200

    [engine/web] Spawn a coq process on websocket connection to the server

--- engine/server/src/main.rs
+++ engine/server/src/main.rs
@@ -1,25 +1,42 @@
+use clap::Parser;
 use futures::future;
 use futures_util::sink::SinkExt;
 use futures_util::{FutureExt, StreamExt, TryFutureExt, TryStreamExt};
+use tokio::process::Command;
 use tokio_util::bytes::Bytes;
 use warp::http::Uri;
 use warp::Filter;
 
+#[derive(Parser, Debug, Clone)]
+pub struct Args {
+    #[arg(long)]
+    vfiles: String,
+    #[arg(long)]
+    modname: String,
+    #[arg(long)]
+    plugin: String,
+}
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
     let static_assets = warp::path("static").and(warp::fs::dir("../web"));
     let root = warp::path::end().map(|| warp::redirect(Uri::from_static("/static/index.html")));
     let script = warp::path("scripts").and(warp::fs::dir("../web"));
     let websocket = warp::path("wss")
         .and(warp::ws())
         .and(warp::path::param())
-        .and_then(ws_handler);
+        .and_then(move |ws, id| ws_handler(args.clone(), ws, id));
     let routes = static_assets.or(root).or(script).or(websocket);
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
 
-pub async fn ws_handler(ws: warp::ws::Ws, id: String) -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(ws.on_upgrade(|socket| client_connection(socket, id)))
+pub async fn ws_handler(
+    args: Args,
+    ws: warp::ws::Ws,
+    id: String,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(ws.on_upgrade(|socket| client_connection(args, socket, id)))
 }
 
 #[derive(Debug)]
@@ -28,11 +45,15 @@ pub enum ConnectionError {
     WARP(warp::Error),
 }
 
-pub async fn client_connection(ws: warp::ws::WebSocket, id: String) {
+pub async fn client_connection(args: Args, ws: warp::ws::WebSocket, id: String) {
     let temp_dir = tempfile::tempdir().unwrap();
     let mut socket_path = temp_dir.path().to_path_buf();
     socket_path.push(std::path::Path::new("socket"));
-    let future = tokio::net::UnixStream::connect(socket_path)
+    let mut vfile_path = std::path::Path::new(&args.vfiles).to_path_buf();
+    vfile_path.push(std::path::Path::new(&format!("{}.v", id)));
+
+    // Bind the unix stream to the websocket stream
+    let unix = tokio::net::UnixStream::connect(socket_path.clone())
         .and_then(|unix_socket| {
             let (unix_sink, unix_stream) = tokio_util::codec::length_delimited::Builder::new()
                 .native_endian()
@@ -59,5 +80,20 @@ pub async fn client_connection(ws: warp::ws::WebSocket, id: String) {
         .unwrap_or_else(|err| {
             log::debug!("Error on connection for {}: {:?}", id, err);
         });
-    future.await
+
+    // Spawn coq
+    let mut cmd = Command::new("coqc")
+        .arg("-R")
+        .arg(args.vfiles)
+        .arg(args.modname)
+        .arg("-noinit")
+        .arg("-indices-matter")
+        .arg("-I")
+        .arg(args.plugin)
+        .arg(vfile_path)
+        .env("COMDIAG_SOCKET", socket_path)
+        .spawn()
+        .expect("Couldn't spawn coqc");
+
+    future::join(unix, cmd.wait().map(|_| ())).map(|_| ()).await
 }
